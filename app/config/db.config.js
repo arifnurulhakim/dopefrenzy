25e5ae9bf8d84ea9cc618b6513b65b74b3186ab9
arifnurulhakim <arifnhakim23@gmail.com>
2023-07-26 14:24:55 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 module.exports = {
-  HOST: "tiny.db.elephantsql.com",
-    USER: "slwiqpwz",
-    PASSWORD: "zOBvmpKtPd00CIFXR-ur8N4hk1vEVC8Z",
-    DB: "slwiqpwz",
+    HOST: "database-1.caaq62uigts9.ap-southeast-1.rds.amazonaws.com",
+    USER: "postgres",
+    PASSWORD: "rqIGlz5x2iFPArVBLXIx",
+    DB: "postgres",
+    // HOST: "user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com",
+    // USER: "shooting-star-main-db-0566c731c8e780280",
+    // PASSWORD: "DacHg9bCc6NERpPRAtYkNBYkxXPJe9",
+    // DB: "shooting-star-main-db-0566c731c8e780280",
     dialect: "postgres",
     dialectOptions: {
-      idle_in_transaction_session_timeout: 1000
+      idle_in_transaction_session_timeout: 1000,
+      
     },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 1000
-  }
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 1000
+    }
 };
